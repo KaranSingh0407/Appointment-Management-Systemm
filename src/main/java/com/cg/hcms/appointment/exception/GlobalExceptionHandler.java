6c2875baff3e4aba8c7241fa51ec919e588c70d3
KaranSingh0407 <ksb04071998@gmail.com>
2020-09-25 23:13:29 +0530

    Global Exception Handled

--- src/main/java/com/cg/hcms/appointment/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cg/hcms/appointment/exception/GlobalExceptionHandler.java
@@ -11,27 +11,23 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(AppointmentNotFoundException.class)
 	public ResponseEntity<?> handleAppointmentNotFoundException(AppointmentNotFoundException exception) {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
-		//To handle Appointment not found exception
 	}
 	
 	@ExceptionHandler(SlotNotAvailableException.class)
 	public ResponseEntity<?> handleSlotNotAvailableException(SlotNotAvailableException exception) {
 		return ResponseEntity.status(HttpStatus.ALREADY_REPORTED).body(exception.getMessage());
-		//To handle slot not available exception
 	}
 
 	@ExceptionHandler(AppointmentAlreadyApprovedException.class)
 	@ResponseStatus(value = HttpStatus.ALREADY_REPORTED, reason = "Appointment Already Approved")
 	public ResponseEntity<?> handleAppointmentAlreadyApprovedException(AppointmentAlreadyApprovedException exception) {
 		return ResponseEntity.status(HttpStatus.ALREADY_REPORTED).body(exception.getMessage());
-		//To handle Appointment already approved exception
 	}
 
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "You have entered invalid data", code = HttpStatus.BAD_REQUEST)
 	public ResponseEntity<?> handleInvalidData(Exception exception) {
 		return ResponseEntity.status(HttpStatus.ALREADY_REPORTED).body(exception.getMessage());	
-		//To handle bad requests
 	}
 	
 }
